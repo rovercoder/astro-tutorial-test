fb9bb97c8c4c832d76514175862ffe9c22a942c4
Nathan Zahra <nathanzahra@hotmail.com>
2025-09-09 17:29:02 +0200

    Upgraded dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astro-tutorial-test-1.netlify.app"
+  site: "https://astro-tutorial-test-1.netlify.app",
+  integrations: [preact()]
 });
